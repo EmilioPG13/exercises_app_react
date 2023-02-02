c9edceaa92a5e471bfd5593862d1a279bc45bc93
Emilio <emilio.pg95@gmail.com>
2023-02-02 17:23:19 -0600

    🐛 Videos are still not displaying

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import { exerciseOptions } from '../utils/fetchData'
 
 function ExerciseVideos( exerciseVideos, name ) {
 
@@ -8,7 +7,7 @@ function ExerciseVideos( exerciseVideos, name ) {
   return (
     <Box sx={{ marginTop: { lg: '200px', xs: '20px' }}} p='20px'>
       <Typography variant='h4' mb='33px'>
-        Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span>  exercise videos
+        Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> exercise videos
       </Typography>
       <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='center'
         sx= {{
@@ -20,10 +19,16 @@ function ExerciseVideos( exerciseVideos, name ) {
           <a
             key={index}
             className='exercise-video'
-            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}            target='_blank'
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target='_blank'
             rel='noreferrer'
           >
-            <img src={item.video.thumbnails[0].url} alt={item.video.title}></img>
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography variant='h5' color='#000'>
+                {item.video.title}
+              </Typography>
+            </Box>
           </a>
         ))}
       </Stack>
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,16 +2,17 @@ import React, { useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/material'
 
-import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData'
-import Detail from '../components/Detail'
-import ExerciseVideos from '../components/ExerciseVideos'
-import SimilarExercises from '../components/SimilarExercises'
-
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
-  const [exerciseDetail, setExerciseDetail] = useState({})
-  const [exerciseVideos, setExerciseVideos] = useState([])
-  const { id } = useParams()
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -23,6 +24,13 @@ const ExerciseDetail = () => {
 
       const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
       setExerciseVideos(exerciseVideosData.contents);
+
+      const targetMuscleExercisesData = await fetchData(`${exerciseDBUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData)
+
+      const equipmentExercisesData = await fetchData(`${exerciseDBUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equipmentExercisesData)
+
     }
 
     fetchExercisesData();
@@ -32,7 +40,7 @@ const ExerciseDetail = () => {
     <Box>
       <Detail exerciseDetail={exerciseDetail}/>
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
-      <SimilarExercises />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises}  />
     </Box>
   )
 }
