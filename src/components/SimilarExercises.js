399a73f521aff984b10f43134c963eb34501ce3e
Emilio <emilio.pg95@gmail.com>
2023-02-03 08:24:54 -0600

    🚀 Adding some extra sections

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,12 +7,20 @@ import Loader from './Loader';
 function SimilarExercises(targetMuscleExercises, equipmentExercises) {
   return (
     <Box sx={{ mt: { lg: '100px', sx: '0' } }}>
-      <Typography variant='h3'>
+      <Typography variant='h3' mb= {5}>
         Exercises that target the same muscle group
       </Typography>
       <Stack direction='row' sx={{ p:'2', position: 'relative' }}>
         {targetMuscleExercises.length ?
           <HorizontalScrollbar data={targetMuscleExercises} />
+        : <Loader />}
+      </Stack>
+      <Typography variant='h3' mb= {5}>
+        Exercises that target the same muscle equipment
+      </Typography>
+      <Stack direction='row' sx={{ p:'2', position: 'relative' }}>
+        {equipmentExercises.length ?
+          <HorizontalScrollbar data={equipmentExercises} />
         : <Loader />}
       </Stack>
     </Box>
